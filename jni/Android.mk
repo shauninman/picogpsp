68efde5b943deff0511a6ce503a7b5b8fd4e385f
twinaphex <libretro@gmail.com>
2014-12-10 04:54:32 +0100

    Add Android JNI files

---
+++ jni/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := retro
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DANDROID_ARM
+LOCAL_ARM_MODE := arm
+endif
+
+#ifeq ($(TARGET_ARCH),x86)
+#LOCAL_CFLAGS +=  -DANDROID_X86
+#endif
+
+#ifeq ($(TARGET_ARCH),mips)
+#LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
+#endif
+
+CORE_DIR := ..
+
+include $(CORE_DIR)/Makefile.common
+
+CPU_ARCH := arm
+
+LOCAL_SRC_FILES    += $(SOURCES_C) $(SOURCES_ASM)
+LOCAL_CFLAGS += -O2 -DARM_ARCH -DNDEBUG -DINLINE=inline -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 $(INCFLAGS)
+
+include $(BUILD_SHARED_LIBRARY)
